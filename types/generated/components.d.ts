2fb3f38086c9a18d8db86b5f5d25c21fb4cef926
S4adShafiq <saadshafiq009@gmail.com>
2025-05-20 12:05:52 +0500

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,97 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ProductSeo extends Struct.ComponentSchema {
+  collectionName: 'components_product_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'alien';
+  };
+  attributes: {
+    keywords: Schema.Attribute.String;
+    metaDescription: Schema.Attribute.Blocks;
+    metaImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    metaTitle: Schema.Attribute.String;
+    openGraphDescription: Schema.Attribute.String;
+    openGraphImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    openGraphTitle: Schema.Attribute.String;
+  };
+}
+
+export interface SharedOpenGraph extends Struct.ComponentSchema {
+  collectionName: 'components_shared_open_graphs';
+  info: {
+    displayName: 'openGraph';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    ogDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 200;
+      }>;
+    ogImage: Schema.Attribute.Media<'images'>;
+    ogTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 70;
+      }>;
+    ogType: Schema.Attribute.String;
+    ogUrl: Schema.Attribute.String;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    canonicalURL: Schema.Attribute.String;
+    keywords: Schema.Attribute.Text;
+    metaDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 160;
+        minLength: 50;
+      }>;
+    metaImage: Schema.Attribute.Media<'images'>;
+    metaRobots: Schema.Attribute.String;
+    metaTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaViewport: Schema.Attribute.String;
+    openGraph: Schema.Attribute.Component<'shared.open-graph', false>;
+    structuredData: Schema.Attribute.JSON;
+  };
+}
+
+export interface SizeSize extends Struct.ComponentSchema {
+  collectionName: 'components_size_sizes';
+  info: {
+    displayName: 'size';
+    icon: 'apps';
+  };
+  attributes: {
+    size: Schema.Attribute.Enumeration<['Small', 'Medium', 'Large']>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'product.seo': ProductSeo;
+      'shared.open-graph': SharedOpenGraph;
+      'shared.seo': SharedSeo;
+      'size.size': SizeSize;
+    }
+  }
+}
